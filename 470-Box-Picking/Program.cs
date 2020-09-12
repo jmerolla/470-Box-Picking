42884cedc70ef0a6aa6b048f0630195a72b3edf3
jmerolla <48492407+jmerolla@users.noreply.github.com>
2020-09-12 18:42:08 -0400

    Add project files.

---
+++ 470-Box-Picking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _470_Box_Picking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
