90e995446821322687cec9a437c5bccc13d3549b
Spencer Dusi <sdusi22@jcu.edu>
2020-09-14 12:04:33 -0400

    Spencer test commit

--- 470-Box-Picking/Program.cs
+++ 470-Box-Picking/Program.cs
@@ -9,6 +9,7 @@ namespace _470_Box_Picking
             Console.WriteLine("Hello World!");
             Console.WriteLine("Jess Commit");
             Console.WriteLine("Graham Commit");
+            Console.WriteLine("Spencer Commit");
             //Jess test from in-progress branch
             //Graham test from in-progress branch
         }
