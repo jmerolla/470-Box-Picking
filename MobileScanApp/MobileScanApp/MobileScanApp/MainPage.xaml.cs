f86ffcca5d132a0c2325cdfef2ceabe7a4e2c75e
jmerolla <jess.merolla@gmail.com>
2020-09-19 02:47:12 -0400

    * MainPage.xaml: * MainPage.xaml.cs: * MobileScanApp.csproj:
    * AveryDennisonLogo.svg.png: @author Jess Merolla
    
    -Added ZXing NuGet packages for scanning functionality.
    -Edited the main page to hold a begin scan button that will eventually
      link to scanning an order
    (Would then open up the rest of the app's functionality)

--- MobileScanApp/MobileScanApp/MobileScanApp/MainPage.xaml.cs
+++ MobileScanApp/MobileScanApp/MobileScanApp/MainPage.xaml.cs
@@ -5,14 +5,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace MobileScanApp
 {
+
+    /*
+     * @author:     Jess Merolla
+     * @date:       9/19/2020
+     * @summary:
+     *
+     * This class initializes the start-up page.
+     * Contains a single button to begin scanning in the initial
+     * order.
+     * 
+     *  
+     *
+     *
+     *
+     */
     public partial class MainPage : ContentPage
     {
+        
         public MainPage()
         {
             InitializeComponent();
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+
+
+            BoxView logoBoxview = new BoxView
+            {
+                Color = Color.Accent,
+                WidthRequest = mainDisplayInfo.Width,
+                HeightRequest = mainDisplayInfo.Height / 12,
+            };
+
+            this.Content = new StackLayout
+            {
+                Children =
+               {
+                   logoBoxview,
+                   scanOrderButton
+               }
+            };
         }
 
         private void Button_Clicked(object sender, EventArgs e)
