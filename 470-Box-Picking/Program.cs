097c75b1e16284accab7b0dccfeca1f28ae1425d
Spencer Dusi <sdusi22@jcu.edu>
2020-09-24 18:05:31 -0400

    Revert "Removed outdated files and solutions unrelated to the mobile scan app"
    This reverts commit 28fcf04076e35d65a08974e8ca2cb079253a5f4e.

---
+++ 470-Box-Picking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _470_Box_Picking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Jess Commit");
+            //Jess test from in-progress branch
+            //Graham test from in-progress branch
+            //Spencer test from in-progress branch
+        }
+    }
+}
