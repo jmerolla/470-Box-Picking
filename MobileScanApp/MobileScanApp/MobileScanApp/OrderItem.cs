144073b2d8520e37e5ab263ce4026e12911c210d
jmerolla <48492407+jmerolla@users.noreply.github.com>
2020-09-29 17:49:37 -0400

    -Added OrderItem.cs class to hold information for each order item -Added OrderListView page to hold a list view of order items (currently empty with no links to it)

---
+++ MobileScanApp/MobileScanApp/MobileScanApp/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileScanApp
+{
+    class OrderItem
+    {
+        public String Name;
+        public String Location;
+        public String barcodeID;
+        public int palletQty;
+        public int cartonQty;
+     
+    }
+}
