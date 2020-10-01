8d4ead06dac54ce57bebf5ec1aa88a226bd753e0
Spencer Dusi <sdusi22@jcu.edu>
2020-10-01 17:15:31 -0400

    Added comparer for barcodes. Under ScanPage class

--- MobileScanApp/MobileScanApp/MobileScanApp/ScanPage.xaml.cs
+++ MobileScanApp/MobileScanApp/MobileScanApp/ScanPage.xaml.cs
@@ -20,6 +20,7 @@ namespace MobileScanApp
      */
     public partial class ScanPage : ContentPage
     {
+        String barCodeRead;
         StackLayout stkMainlayout;
         public ScanPage()
         {
@@ -43,12 +44,41 @@ namespace MobileScanApp
                     Device.BeginInvokeOnMainThread(async () => {
                         await Navigation.PopModalAsync();
                         await DisplayAlert("Scanned Barcode", result.Text + " , " + result.BarcodeFormat + " ," + result.ResultPoints[0].ToString(), "OK");
+                            barCodeRead = result.Text;
+                        if (barCodeMatcher()) //If the scan matches the barcodes, display the alert.
+                        {
+                            await DisplayAlert("Barcode Matches", result.Text + " , " + result.BarcodeFormat + " ," + result.ResultPoints[0].ToString(), "OK");
+                        }
                     });
                 };
                 await Navigation.PushModalAsync(scanPage);
             };
             stkMainlayout.Children.Add(btnScan);
             Content = stkMainlayout;
+          
+        }
+
+        /**
+         * Edited by Spencer Dusi 9-29-20.
+         * This Class takes the barcodes and test to see if it matches the one on the sheet.
+         * If it matches we set our boolean true allowing the pop up to happen.
+         * TODO - Link the barcode numbers from the list.
+         */
+        public Boolean barCodeMatcher() {
+            var BarCodes = new List<string>()
+                {
+                   "5561600741",
+                   "1204403891",
+                };
+            //Testing to see if the result of the capture match our barcodes.
+            //Loops through the array of barcodes scanned in. If true the pop up activates.
+            for (int i = 0; i < BarCodes.Count; i++) {
+                if (barCodeRead.Contains(BarCodes[i]))
+                {
+                    return true;
+                }
+            }
+                return false;
         }
     }
 }
